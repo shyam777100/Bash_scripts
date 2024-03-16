8c2d9328805d8210d4ebfe3a551110836b8486a1
Shyamsundar Dattatraya Dudhbhate <shyam777100@gmail.com>
2024-03-16 10:49:58 +0530

    beginner_scripts

---
+++ 02_comments.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+echo "Checking Comments"
+
+echo " #Hash is used for single line comment"
+
+echo "For multiline comment, we need to start with :-  <<comment ------ comment "
+
+
+echo "This is the way to add comment in bash scripting"
