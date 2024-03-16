ae30b4dd7066baa343e2fbb71ff1f4a928db357f
Shyamsundar Dattatraya Dudhbhate <shyam777100@gmail.com>
2024-03-16 10:54:48 +0530

    Update 02_comments.sh

--- 02_comments.sh
+++ 02_comments.sh
@@ -6,4 +6,4 @@ echo " #Hash is used for single line comment"
 echo "For multiline comment, we need to start with :-  <<comment ------ comment "
 
 
-echo "This is the way to add comment in bash scripting"
+echo "This is the way to we can add comment in bash scripting"
