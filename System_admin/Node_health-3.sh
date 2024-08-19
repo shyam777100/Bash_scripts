66823b0c15373f86ba79464a763fcf9a81d7ea3e
Shyamsundar Dattatraya Dudhbhate <shyam777100@gmail.com>
2024-08-19 08:41:38 +0530

    190824

---
+++ System_admin/Node_health-3.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+#######################
+#Author : Shyamsundar D
+#Date : 08/08/2024
+#
+# This script outputs the node health
+#
+#Version: v3
+#######################
+
+#set -x # debug mode
+#set -e # exit the script when there is an error
+#set -o pipefail
+
+# Instead of writing seprate commands we can write in a single line
+
+set -exo pipefaill
